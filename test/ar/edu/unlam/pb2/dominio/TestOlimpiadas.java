1c702343514108c0ec33a110f0b5fd16a3368cd3
Gonzalo Greatti <gonza_lauty@outlook.com>
2024-06-13 19:26:00 -0300

    se agrego el parcial de olimpiadas

---
+++ test/ar/edu/unlam/pb2/dominio/TestOlimpiadas.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.dominio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestOlimpiadas {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
